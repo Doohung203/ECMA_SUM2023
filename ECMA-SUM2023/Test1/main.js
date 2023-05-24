b31d12f48c1744e207d50a7f72312e540f1bfeda
Doo03 <Doohung2003@gmail.com>
2023-05-24 11:01:21 +0700

    test

--- ECMA-SUM2023/Test1/main.js
+++ ECMA-SUM2023/Test1/main.js
@@ -1,49 +1,43 @@
 const product = ["mescedes", "audi", "ford", "lamborghini"];
 
-// console.log(product);
-// const showProduct = () =>{
-//     if(product == ""){
-//         console.log("không có sản phẩm để hiển thị")
-//     }else{
-//         console.log(product)
-//     }
-// }
-// showProduct()
-
-// const addProduct = () =>{
-//     const nameProduct = prompt("Nhập tên sản phẩm");
-//     // do{
-//     //     nameProduct = prompt("Nhập lại tên sản phẩm");
-//     // }while(nameProduct.length < 5)
-//     if(nameProduct.length < 5){
-//         nameProduct = prompt("Nhập lại tên sản phẩm");
-//     }else{
-//         product.push(nameProduct);
-//     }
-//    showProduct()
-// }
-// addProduct()
-
-// const removeProduct = () =>{
-//     const nameProductRemove = prompt("Nhập tên sản phẩm cần xóa");
-//     if(nameProductRemove == product.length){
-//         nameProductRemove = product.filter(item => item !== product.length)
-//         console.log(product);
-//     }else{
-//         console.log("Không tìm thấy sản phẩm cần xóa")
-//     }
-// }
-// removeProduct()
-const removeProduct = (product) => {
-    const nameProductRemove = prompt("Nhập tên sản phẩm cần xóa");
-    const index = product.index;
-    if (index !== -1) {
-        product.splice(index, 1);
-        console.log(product)
+console.log(product);
+const showProduct = () =>{
+    if(product == ""){
+        console.log("không có sản phẩm để hiển thị")
     }else{
-        console.log("không tìm thấy sản phẩm cần xóa")
+        console.log(product)
+    }
+}
+showProduct()
+
+function addProduct() {
+    var productName;
+    do {
+      productName = prompt("Nhập tên sản phẩm:");
+      if (productName.length < 5) {
+        console.log("Nhập lại tên sản phẩm ");
+      }
+    } while (productName.length < 5);
+  
+    product.push(productName);
+    console.log(product);
+  }
+addProduct()
+
+const removeProduct = () =>{
+    const nameProductRemove = prompt("Nhập tên sản phẩm cần xóa");
+    const index = product.indexOf(nameProductRemove);
+    if(index !== -1){
+        product.splice(index, 1);
+        console.log(product)   
+    }else{
+        console.log("Không tìm thấy sản phẩm cần xóa")
     }
 }
 removeProduct()
-
-console.log(product)
+function removeAllProduct() {
+    product = [];
+    console.log("Đã xóa toàn bộ sản phẩm trong mảng.");
+    showProduct();
+  }
+z
